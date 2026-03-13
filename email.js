341844625d6354b064e607b6b0eb88f1634d87bd
Mehul <mehuljadhav44770@gmail.com>
2026-03-13 12:18:01 +0530

    setup resend email service

--- email.js
+++ email.js
@@ -3,23 +3,20 @@ require("dotenv").config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-async function sendEmail(to, subject, text) {
-
+async function sendEmail(to, subject, text, html = "") {
   try {
 
     const response = await resend.emails.send({
-      from: "QuickFix <onboarding@resend.dev>",
+      from: "Fixofix <noreply@fixofix.online>",   // your verified domain
       to: to,
       subject: subject,
-      html: `<p>${text}</p>`
+      html: html || `<p>${text}</p>`
     });
 
-    console.log("✅ Email sent:", response);
-
+    console.log("Email sent:", response);
   } catch (error) {
-    console.error("❌ Email error:", error);
+    console.error("Email error:", error);
   }
-
 }
 
 module.exports = sendEmail;
